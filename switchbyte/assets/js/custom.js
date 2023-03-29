4ebd7247d320fc71759d26a2bd17d99acdea67b2
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-03-29 23:34:33 +0500

    Add files via upload

---
+++ switchbyte/assets/js/custom.js
@@ -0,0 +1,103 @@
+$('.Affiliated-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+$('.about-us-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+
+
+
+
+let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".itembox");
+let boxFancy = document.querySelectorAll(".fancybox");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k < itemBox.length; k++) {
+      itemBox[k].classList.remove("active");
+      itemBox[k].classList.add("hide");
+
+      if (
+        itemBox[k].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        itemBox[k].classList.remove("hide");
+        itemBox[k].classList.add("active");
+      }
+    }
+    for (let m = 0; m < boxFancy.length; m++) {
+      boxFancy[m].classList.remove("active");
+      Fancybox.bind("[data-fancybox].active", {
+        groupAll: false
+      });
+      if (
+        boxFancy[m].getAttribute("data-item") == dataFilter ||
+        dataFilter == "all"
+      ) {
+        boxFancy[m].classList.add("active");
+        Fancybox.bind("[data-fancybox].active", {
+          groupAll: true
+        });
+      }
+    }
+  });
+}
+
+
+AOS.init();
+
