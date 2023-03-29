20137feb1db0a8773539c4f7cb2b4a33aed44e18
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-03-29 23:20:35 +0500

    Add files via upload

---
+++ project-four/public/assets/js/firebase.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVnP5u50FsE85TCdaZJWCHUCguR8sOqbk",
+    authDomain: "hackathon-module-1.firebaseapp.com",
+    projectId: "hackathon-module-1",
+    storageBucket: "hackathon-module-1.appspot.com",
+    messagingSenderId: "440163450654",
+    appId: "1:440163450654:web:65761c600555481751930f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
