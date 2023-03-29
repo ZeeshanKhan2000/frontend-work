a681cdd0cd1c62f538f7ac0329cb74ab396393a6
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-03-29 23:22:19 +0500

    Add files via upload

---
+++ project-one/assets/js/custom.js
@@ -0,0 +1,96 @@
+// Animations call 
+
+// AOS.init({
+//     duration: 1200,
+//     once: true,
+// })
+
+// new WOW().init();
+
+
+// end Animation 
+
+
+
+
+
+
+// counter start at six-wrap class 
+// JS 
+
+// $(window).on('scroll', function () {
+//     if ($(window).scrollTop() >= $('.six-wrap').offset().top + $('.six-wrap').outerHeight() - window.innerHeight){
+//         counterrr();
+//     }
+// });
+
+
+// function counterrr() {
+//     $('.counter').each(function () {
+//         $(this).prop('Counter', 0).animate({
+//             Counter: $(this).text()
+//         }, {
+//             duration: 3000,
+//             easing: 'swing',
+//             step: function (now) {
+//                 $(this).text(Math.ceil(now));
+//             }
+//         });
+//     });
+//     $('.counter1').removeClass('counter');
+// }
+
+// close 
+
+
+
+
+
+
+
+
+
+
+
+// scroll behavior 
+// window.scroll({
+//     behavior: 'smooth'
+// });
+
+
+
+
+// collapse hide on complete body click expert header-menulink-main 
+
+// $(document).on('click', function (event) {
+//     if (!$(event.target).closest('.header-menulinks-main').length) {
+//         $('.collapse').collapse('hide');
+//     }
+// });
+
+
+// inner click closing in  industry-inner class 
+
+// $(".industry-inner").click(function () {
+//     $('.collapse').collapse('hide');
+// });
+
+
+
+$('.zeeshan').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dot: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
