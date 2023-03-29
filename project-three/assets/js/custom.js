b288b0eb87d150e56b55ad638092b2095662217a
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2023-03-29 23:23:36 +0500

    Add files via upload

---
+++ project-three/assets/js/custom.js
@@ -0,0 +1,58 @@
+$(".banner-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$(".product-slider").owlCarousel({
+  loop: true,
+  margin: 60,
+  nav: true,
+  responsive: {
+    0: {
+      items: 4,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+$(".our-menu-slider ").owlCarousel({
+  loop: true,
+  margin: 60,
+  nav: true,
+  responsive: {
+    0: {
+      items: 4,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+
+// *****************************product detail ****************
+
+
+
+
+
